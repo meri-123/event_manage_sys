07bacfa68e8d2d29edf9a816418556275037a908
meri-123 <merinakhatun740@gmail.com>
2023-08-19 19:38:08 +0530

    second commit

--- app/models/management.rb
+++ app/models/management.rb
@@ -1,7 +1,12 @@
 class Management < ApplicationRecord
     has_many :comments, dependent: :destroy
+    has_one_attached :image
 
     validates :name, presence: true
     validates :email, presence: true
     validates :contact, presence: true
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["name"]
+    end
 end
